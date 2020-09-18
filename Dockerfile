623448ac4d1531b4b54911b0e9c23223fec22031
Xiaotian Duan <xduan7@gmail.com>
2020-09-18 13:23:12 -0500

    switch to wget for downloading

--- Dockerfile
+++ Dockerfile
@@ -11,14 +11,14 @@ RUN \
     # reference: https://www.ncbi.nlm.nih.gov/books/NBK409171/
     echo "installing dependencies and tools ... " && \
     apt-get -y -m -q update && \
-    apt-get -y -q install libidn11 libnet-perl libgomp1 liblist-moreutils-perl perl-doc curl elfutils && \
+    apt-get -y -q install libidn11 libnet-perl libgomp1 liblist-moreutils-perl perl-doc wget elfutils && \
     rm -rf /var/lib/apt/lists/* && \
     # download ncbi blast+ binaries (including rpsbproc)
     echo "downloading ncbi blast+ binaries (including rpsbproc) ... " && \
     mkdir -p /root/tmp && \
     cd /root/tmp && \
-    curl -s ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${version}/ncbi-blast-${version}+-x64-linux.tar.gz | tar xzf -  && \
-    curl -s ftp://ftp.ncbi.nlm.nih.gov/pub/mmdb/cdd/rpsbproc/RpsbProc-x64-linux.tar.gz | tar xzf - && \
+    wget -q ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${version}/ncbi-blast-${version}+-x64-linux.tar.gz && tar -xzf ncbi-blast-${version}+-x64-linux.tar.gz && \
+    wget -q ftp://ftp.ncbi.nlm.nih.gov/pub/mmdb/cdd/rpsbproc/RpsbProc-x64-linux.tar.gz && tar -xzf RpsbProc-x64-linux.tar.gz && \
     # copy the binaries over to /blast
     mkdir -p /blast && \
     cp -r ncbi-blast-${version}+/* /blast && \
@@ -27,17 +27,17 @@ RUN \
     echo "download the data files for rpsbproc ... " && \
 	mkdir -p /blast/data && \
 	cd /blast/data && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdtrack.txt -o cdtrack.txt && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/bitscore_specific.txt -o bitscore_specific.txt && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/family_superfamily_links -o family_superfamily_links && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz -o cddid.tbl.gz && gzip -d cddid.tbl.gz && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddannot.dat.gz -o cddannot.dat.gz && gzip -d cddannot.dat.gz && \
-	curl -s ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddannot_generic.dat.gz -o cddannot_generic.dat.gz && gzip -d cddannot_generic.dat.gz && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdtrack.txt && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/bitscore_specific.txt && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/family_superfamily_links && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz && gzip -d cddid.tbl.gz && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddannot.dat.gz && gzip -d cddannot.dat.gz && \
+	wget -q ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddannot_generic.dat.gz && gzip -d cddannot_generic.dat.gz && \
 	# clean up and finalize the blast folder
     echo "cleaning up ... " && \
 	rm -rf /root/tmp/ && \
 	mkdir -p /blast/db /blast/queries /blast/results && \
-	apt-get -y -q remove --purge --auto-remove curl
+	apt-get -y -q remove --purge --auto-remove wget
 
 COPY --from=edirect /usr/local/ncbi/edirect /root/edirect
 ENV PATH="/blast/bin:/root/edirect:${PATH}"
--- build.sh
+++ build.sh
@@ -38,54 +38,52 @@ fi
 
 # execute the test script for validation
 # test docker image with rpstblastn and rpsbprc
-if ${TEST_FLAG}; then
+# if ${TEST_FLAG}; then
 
-  # the following code segment requires CD to run, and only test rpstblastn and rpsbproc
+#   # the following code segment requires CD to run, and only test rpstblastn and rpsbproc
+#   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#   PROJECT_DIR="$( dirname "$( dirname "${SCRIPT_DIR}" )" )"
 
-  # SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  # PROJECT_DIR="$( dirname "$( dirname "${SCRIPT_DIR}" )" )"
+#   TEST_NUCLEOTIDE_NAME="AXOC01000064"
+#   LOCAL_QUERIES_DIR="${SCRIPT_DIR}/queries"
+#   LOCAL_RESULTS_DIR="${SCRIPT_DIR}/results"
+#   LOCAL_DB_DIR="${PROJECT_DIR}/data/interim/CDD"
+#   DB_NAME="Cdd"
+#   DOCKER_QUERIES_DIR="/blast/queries"
+#   DOCKER_RESULTS_DIR="/blast/results"
+#   DOCKER_DB_DIR="/blast/db"
 
-  # TEST_NUCLEOTIDE_NAME="AXOC01000064"
-  # LOCAL_QUERIES_DIR="${SCRIPT_DIR}/queries"
-  # LOCAL_RESULTS_DIR="${SCRIPT_DIR}/results"
-  # LOCAL_DB_DIR="${PROJECT_DIR}/data/interim/CDD"
-  # DB_NAME="Cdd"
-  # DOCKER_QUERIES_DIR="/blast/queries"
-  # DOCKER_RESULTS_DIR="/blast/results"
-  # DOCKER_DB_DIR="/blast/db"
+#   echo "Running rpstblastn with example nucleotide ..."
+#   mkdir -p "${LOCAL_RESULTS_DIR}"
+#   rm "${LOCAL_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn || true
+#   rm "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt || true
 
-  # echo "Running rpstblastn with example nucleotide ..."
-  # mkdir -p "${LOCAL_RESULTS_DIR}"
-  # rm "${LOCAL_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn || true
-  # rm "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt || true
+#   docker run --rm \
+#       -v "${LOCAL_QUERIES_DIR}":"${DOCKER_QUERIES_DIR}":ro \
+#       -v "${LOCAL_RESULTS_DIR}":"${DOCKER_RESULTS_DIR}":rw \
+#       -v "${LOCAL_DB_DIR}":"${DOCKER_DB_DIR}":ro \
+#       "${IMAGE_PREFIX}"blast:"${VERSION}" \
+#       rpstblastn \
+#           -query "${DOCKER_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".fna \
+#           -db "${DOCKER_DB_DIR}"/"${DB_NAME}" \
+#           -evalue 0.01 \
+#           -outfmt 11 \
+#           -out "${DOCKER_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn
 
-  # docker run --rm \
-  #     -v "${LOCAL_QUERIES_DIR}":"${DOCKER_QUERIES_DIR}":ro \
-  #     -v "${LOCAL_RESULTS_DIR}":"${DOCKER_RESULTS_DIR}":rw \
-  #     -v "${LOCAL_DB_DIR}":"${DOCKER_DB_DIR}":ro \
-  #     "${IMAGE_PREFIX}"blast:"${VERSION}" \
-  #     rpstblastn \
-  #         -query "${DOCKER_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".fna \
-  #         -db "${DOCKER_DB_DIR}"/"${DB_NAME}" \
-  #         -evalue 0.01 \
-  #         -outfmt 11 \
-  #         -out "${DOCKER_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn
+#   echo "Parsing results from rpstblastn  ..."
+#   mv "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn "${LOCAL_QUERIES_DIR}"
+#   docker run --rm \
+#       -v "${LOCAL_QUERIES_DIR}":"${DOCKER_QUERIES_DIR}":ro \
+#       -v "${LOCAL_RESULTS_DIR}":"${DOCKER_RESULTS_DIR}":rw \
+#       -v "${LOCAL_DB_DIR}":"${DOCKER_DB_DIR}":ro \
+#       "${IMAGE_PREFIX}"blast:"${VERSION}" \
+#       rpsbproc \
+#           -i "${DOCKER_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn \
+#           -o "${DOCKER_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt \
+#           -e 0.01 \
+#           -m rep
 
-  # echo "Parsing results from rpstblastn  ..."
-  # mv "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn "${LOCAL_QUERIES_DIR}"
-  # docker run --rm \
-  #     -v "${LOCAL_QUERIES_DIR}":"${DOCKER_QUERIES_DIR}":ro \
-  #     -v "${LOCAL_RESULTS_DIR}":"${DOCKER_RESULTS_DIR}":rw \
-  #     -v "${LOCAL_DB_DIR}":"${DOCKER_DB_DIR}":ro \
-  #     "${IMAGE_PREFIX}"blast:"${VERSION}" \
-  #     rpsbproc \
-  #         -i "${DOCKER_QUERIES_DIR}"/"${TEST_NUCLEOTIDE_NAME}".asn \
-  #         -o "${DOCKER_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt \
-  #         -e 0.01 \
-  #         -m rep
+#   echo "Parsed results (in concise layout):"
+#   cat "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt
 
-  # echo "Parsed results (in concise layout):"
-  # cat "${LOCAL_RESULTS_DIR}"/"${TEST_NUCLEOTIDE_NAME}".txt
-  pass
-
-fi
+# fi
